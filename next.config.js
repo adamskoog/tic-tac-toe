73e2b25f592e8af6a9e8b3204dc308594b8784b4
adamskoog <adamskoog@outlook.com>
2022-09-22 18:53:21 -0500

    Add cache control for font. Add nvmrc file.

--- next.config.js
+++ next.config.js
@@ -2,6 +2,22 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+
+  async headers() {
+    return [
+      {
+        source: "/fonts/crystal-radio-kit.regular.ttf",
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "public, max-age=31536000, immutable",
+          },
+        ],
+      },
+    ];
+  },
 }
+
+
 
 module.exports = nextConfig
