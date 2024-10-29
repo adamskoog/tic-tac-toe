2d8a5ac51b02d67efab1019e2609790328e55fa8
adamskoog <adamskoog@outlook.com>
2024-10-29 15:03:53 -0500

    chore - update packages and metadata.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,23 @@
 import React from 'react';
+import { Metadata, Viewport } from 'next'
 import StyledComponentsRegistry from '@/util/registry';
+
+export const viewport: Viewport = {
+  themeColor: '#283739',
+  width: 'device-width',
+  initialScale: 1
+}
+
+export const metadata: Metadata = {
+  description: 'A tic-tac-toe game to play for free!',
+  keywords: ['tic-tac-toe', 'Game'],
+}
 
 export default function RootLayout({ children }: { children: any }) {
 
   return (
     <html lang="en">
       <head>
-        <meta
-          name="viewport"
-          content="minimum-scale=1, initial-scale=1, width=device-width, user-scalable=no"
-        />
         <link
             rel="preload"
             href="/fonts/crystal-radio-kit.regular.ttf"
--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,8 @@ import pjson from '@/package.json'
 
 export const metadata: Metadata = {
     title: pjson.appTitle,
+    authors: [ { name: 'Adam Skoog', url: pjson.repository }],
+    creator: 'Adam Skoog',
 }
 
 export default async function Page() {
